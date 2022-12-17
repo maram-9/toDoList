ba48452be6662ad2df31dc48fe3ef261b56551ac
Marzena Malka <marzena.malka@gmail.com>
2022-12-17 12:16:06 +0100

    toogle task done

--- js/script.js
+++ js/script.js
@@ -27,6 +27,11 @@
                 render();
         }
 
+        const toggleTaskDone = (taskIndex => {
+          tasks[taskIndex].done = !tasks[taskIndex].done;
+          render();
+        })
+
         const render = () => {
             let htmlString = "";
 
@@ -53,6 +58,14 @@
             removeButtons.forEach((removeButton, index) => {
               removeButton.addEventListener("click", () => {
                 removeTask(index);
+            });
+          });
+
+          const toggleDoneButtons = document.querySelectorAll (".js-done");
+           
+          toggleDoneButtons.forEach((toogleDoneButton, index) => {
+              toogleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
             });
           });
         };
